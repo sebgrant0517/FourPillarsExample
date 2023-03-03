eab850540e13f20c46f2f50d724e6756292d403c
sebgrant0517 <122310912+sebgrant0517@users.noreply.github.com>
2023-03-03 16:33:21 -0500

    Uploaded FourPillarsExample project
    This project contains examples of the four pillars, described in the comments.

---
+++ FourPillarsExample/src/java/FrenchBulldog.java
@@ -0,0 +1,14 @@
+//FrenchBulldog extends Dog, which is a form of Inheritance. It gets the previous
+//methods without having to rewrite them.
+public class FrenchBulldog extends Dog{
+    public FrenchBulldog(){
+        sound = "Rorf";
+        name = "French Bulldog";
+    }
+
+    //This is an overloaded constructor, a form of Polymorphism
+    public FrenchBulldog(String n){
+        sound = "Rorf";
+        name = n;
+    }
+}
